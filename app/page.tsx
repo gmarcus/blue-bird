e090080c45f049de4e9661513f0d1ca2b712795f
Glenn Marcus <glenn@cliqconsulting.com>
2023-08-21 04:13:15 -0400

    Lesson 15 end

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { cookies } from 'next/headers'
 import AuthButtonServer from './auth-button-server';
 import { redirect } from 'next/navigation';
 import NewTweet from './new-tweet';
+import Likes from './likes';
 
 export default async function Home() {
 
@@ -15,18 +16,27 @@ export default async function Home() {
   }
 
   // We have a session, so let's show some tweets
-  const { data: tweets } = await supabase.from('tweets').select('*, profiles(*)')
+  const { data } = await supabase
+    .from('tweets')
+    .select('*, profiles(*), likes(*)')
+
+  const tweets = data?.map(tweet => ({
+    ...tweet,
+    user_has_liked_tweet: tweet.likes.find(like => like.user_id === session.user.id),
+    likes: tweet.likes.length
+  })) ?? []
 
   return (
     <>
       <AuthButtonServer />
       <NewTweet />
       {tweets?.map((tweet) => (
-        <div key={tweet.id}>
+        <div className="border" key={tweet.id}>
           <p>
             {tweet.profiles?.name} {tweet.profiles?.username}
           </p>
           <p>{tweet.title}</p>
+          <Likes tweet={tweet} />
         </div>
       ))}
     </>
