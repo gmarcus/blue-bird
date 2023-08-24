f6f3895182fc66522a1ccea5f0cf9fe91e7ded2c
Glenn Marcus <glenn@cliqconsulting.com>
2023-08-24 13:56:57 -0400

    Lesson 18 end

--- app/tweets.tsx
+++ app/tweets.tsx
@@ -1,8 +1,28 @@
 'use client'
-import { experimental_useOptimistic as useOptimistic } from "react"
+import { useEffect, experimental_useOptimistic as useOptimistic } from "react"
 import Likes from "./likes"
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
+import { useRouter } from "next/navigation"
 
 export default function Tweets({ tweets }: { tweets: TweetWithAuthor[] }) {
+
+    const supabase = createClientComponentClient()
+    const router = useRouter()
+
+    useEffect( () => {
+        const channel = supabase.channel("realtime tweets").on("postgres_changes", {
+            event: '*',
+            schema: 'public',
+            table: 'tweets',
+        }, (payload) => {
+            console.log(payload)
+            router.refresh()
+        })
+        .subscribe()
+        return () => {
+            supabase.removeChannel(channel)
+        }        
+    }, [supabase, router])
 
     const [optimisticTweets, addOptimisticTweet]  = useOptimistic<TweetWithAuthor[], TweetWithAuthor>(
         tweets,
